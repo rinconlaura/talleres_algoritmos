2fc26cebfea537e41f950ad97fb9f708d95b329d
Laura Sofia Rincón <109982216+rinconlaura@users.noreply.github.com>
2022-08-07 21:02:28 -0500

    Revert "taller2"
    This reverts commit 9a8a942fde5c8813888d9c86be725f4d08ff728c.

--- taller estructuras secuenciales/punto14.py
+++
@@ -1,9 +0,0 @@
-lan=int(input("ingrese el valor de la lectura anterior:"))
-lac=int(input("ingrese el valor de la lectura actual:"))
-kl=int(input("ingrese el costo del kilovatio:"))
-total= (lac-lan)*kl
-total1= (lan-lac)*kl
-if lac > lan:
-    print("el monto a pagar por consumo es:",total)
-elif lac < lan:
-    print("el monto a pagar por consumo es:",total1)
